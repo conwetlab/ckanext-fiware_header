d3090463a08c044e641b60a85fd98ef9f4180748
Aitor Magán <amagan@conwet.com>
2014-09-15 17:49:56 +0200

    Add License

--- ckanext/fiware_header/plugin.py
+++ ckanext/fiware_header/plugin.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2014 CoNWeT Lab., Universidad Politécnica de Madrid
+
+# This file is part of CKAN FIWARE Header Extension.
+
+# CKAN FIWARE Header Extension is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# CKAN FIWARE Header Extension is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with CKAN FIWARE Header Extension Extension.  If not, see <http://www.gnu.org/licenses/>.
+
 import ckan.plugins as p
 import ckan.plugins.toolkit as tk
 
